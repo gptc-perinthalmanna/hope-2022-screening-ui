2a7f267e5545bde50e26b83e5fc83bd2d9af9253
Amjed Ali K <amjedmgm@gmail.com>
2022-04-03 14:12:10 +0530

    Added Nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import { AppProps } from 'next/app'
-
-import '../styles/index.css'
+import React from "react";
+import { AppProps } from "next/app";
+import NextNProgress from "nextjs-progressbar";
+import "../styles/index.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
-}
+  return (
+    <>
+      <NextNProgress />
+      <Component {...pageProps} />
+    </>
+  );
+};
 
-export default MyApp
+export default MyApp;
