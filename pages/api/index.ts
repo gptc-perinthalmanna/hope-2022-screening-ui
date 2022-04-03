3c4562880aafd36580c3a4be5e5ab4b0999980e4
Amjed Ali K <amjedmgm@gmail.com>
2022-04-03 14:04:30 +0530

    Removed API folder

--- pages/api/index.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function api(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({
-    message: "Hello World",
-  });
-}
